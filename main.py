9ccef4322260b1423ad881244826907d41c039e8
23Edu4rd0 <23eduardoviana@gmail.com>
2025-01-21 17:24:42 -0300

    primeiro commit

---
+++ main.py
@@ -0,0 +1,31 @@
+alphabet = [' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def caesar(original_text, shift_amount, encode_or_decode):
+    output_text = ""
+
+    for letter in original_text:
+        if letter == ' ':
+            output_text += letter
+        elif letter in alphabet:
+            if encode_or_decode == "decode":
+                shift_amount *= -1
+            shifted_position = (alphabet.index(letter) + shift_amount) % len(alphabet)
+            output_text += alphabet[shifted_position]
+        else:  
+            output_text += letter
+
+    print(f"Here is the {encode_or_decode}d result: {output_text}")
+
+
+should_continue = True
+
+while should_continue:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    caesar(original_text=text, shift_amount=shift, encode_or_decode=direction)
+
+    restart = input("Do you wanna continue? Type 'yes' or 'no' ").lower()
+    if restart == "no":
+        should_continue = False
+        print("Goodbye")
